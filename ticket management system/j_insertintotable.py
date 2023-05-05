f96846cdc4fc963bc379a066ebeed705694dbf3d
sibi15 <99868966+sibi15@users.noreply.github.com>
2023-05-05 13:52:42 +0530

    Update j_insertintotable.py

--- ticket management system/j_insertintotable.py
+++ ticket management system/j_insertintotable.py
@@ -1,12 +1,9 @@
 # insert into table code:
-
-
 # connection object creation: 
 import mysql.connector
 import time
-
 con=mysql.connector.connect(host='localhost', user='root', password='', database='test')
-time.sleep(2)
+
 if con.is_connected():
       print('\nSuccessful Connection. ')
 
@@ -14,15 +11,11 @@ cur1=con.cursor()
 queryquery=("drop table random")
 cur1.execute(queryquery)
 con.commit()
-time.sleep(2)
 print('Table dropped.')
       
-
-
 # table deleted after each ticket_id append check, so create again before inserting values:
 cur=con.cursor()
 cur.execute("create table random(ticket_id varchar(3) primary key not null, date varchar(10), time varchar(8))")
-time.sleep(2)
 print('Table created successfully')
 
 # lists for the ID:
@@ -38,22 +31,14 @@ for i in range(1,6):
       numbers.append(i)
 print(numbers)
 
-
-
 # main (run once (i) so that only once all gets appended):
-ticket_id='%s%s'                           # can use f STRINGS
+ticket_id='%s%s'
 for j in letters:
       for k in numbers:
             ticket_id=ticket_id%(j,k)
-            query=("insert into random values('{}','','')".format(ticket_id))    # command done here, as we have to replace the placeholders as it can only be replaced once.
-            cur1=con.cursor()                                                      #|
-            cur1.execute(query)                                                    #v
+            query=("insert into random values('{}','','')".format(ticket_id))    # command done here, as we have to replace the placeholders as it can only be replaced once
+            cur1=con.cursor()
+            cur1.execute(query)
             con.commit()                                                           
-            ticket_id='%s%s'                                                       # HERE
-            #print(ticket_id)
-
-
-
-
-time.sleep(2)
+            ticket_id='%s%s'
 print('\nRows added successfully.')
